62bd7b913156e1eba9d74b6ff26a1c2df5af0f28
Christian Bell <christiantcbell@gmail.com>
2019-07-09 17:42:56 +0100

    added serving of static page in root directory

--- lib/botiful.ex
+++ lib/botiful.ex
@@ -7,6 +7,11 @@ defmodule Botiful do
 
   plug Plug.Logger
 
+  plug Plug.Static,
+    at: "/",
+    from: {:botiful, "priv/static"},
+    only: ~w{index.html}
+
   plug :match
   plug :dispatch
 
@@ -14,5 +19,15 @@ defmodule Botiful do
     send_resp(conn, :ok, "world")
   end
 
+  get "/" do
+    conn
+    |> put_resp_header("content-type", "text/html; charset=utf-8")
+    |> send_file(:ok, root())
+  end
+
   match(_, do: send_resp(conn, :not_found, "oh no!"))
+
+  defp root do
+    Enum.join([:code.priv_dir(:botiful), "/static/index.html"])
+  end
 end
