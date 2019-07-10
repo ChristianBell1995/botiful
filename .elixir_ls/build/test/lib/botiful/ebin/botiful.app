0d86dbd2d0df101390f3f41c6b7139c9d43a0e99
Christian Bell <christiantcbell@gmail.com>
2019-07-10 17:42:08 +0100

    intro to genserver call and casts

--- .elixir_ls/build/test/lib/botiful/ebin/botiful.app
+++ .elixir_ls/build/test/lib/botiful/ebin/botiful.app
@@ -4,7 +4,8 @@
                              plug_cowboy,browser]},
               {description,"botiful"},
               {modules,['Elixir.Botiful','Elixir.Botiful.AgentPlug',
-                        'Elixir.Botiful.Application','Elixir.Botiful.Render']},
+                        'Elixir.Botiful.Application','Elixir.Botiful.Render',
+                        'Elixir.Botiful.Render.Engine']},
               {registered,[]},
               {vsn,"0.1.0"},
               {mod,{'Elixir.Botiful.Application',[]}}]}.
---
+++ lib/botiful/render/engine.ex
@@ -0,0 +1,31 @@
+defmodule Botiful.Render.Engine do
+  use GenServer
+
+  # Public API (Client)
+
+  def start_link(url) do
+    GenServer.start_link(__MODULE__, url)
+  end
+
+  def url(pid) do
+    GenServer.call(pid, :url)
+  end
+
+  def swap_url(pid, new_url) do
+    GenServer.cast(pid, {:swap_url, new_url})
+  end
+
+  # Serverside Callbacks
+
+  def init(url) do
+    {:ok, %{url: url}}
+  end
+
+  def handle_call(:url, _from, state) do
+    {:reply, state.url, state}
+  end
+
+  def handle_cast({:swap_url, new_url}, _state) do
+    {:noreply, %{url: new_url}}
+  end
+end
